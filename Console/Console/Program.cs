fc87125e52cebb305329acfb2912f30a26c78ba2
LinasBushina <xmysteryshadow@gmail.com>
2017-09-12 22:10:00 +0300

    New file console

---
+++ Console/Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "123";
+            int number = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                number = number * 10 + str[i] - 0x30;
+            }
+            System.Console.WriteLine(number);
+        }
+    }
+}
