1b3530aac62494dd1387f15a5b91f626b3ee5712
LinasBushina <xmysteryshadow@gmail.com>
2017-09-13 16:08:17 +0300

    Add home task. File NumToStr

---
+++ Lessons/NumToStr/NumToStr/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumToStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 123;
+            int flag = 0;
+            string str = "";
+            while(num>0)
+            {
+                flag = num % 10;
+                str += flag;
+                num /= 10;
+            }
+            string strdubl = "";
+            for (int i = str.Length - 1; i >= 0  ; i-- )
+            {
+                strdubl += str[i];
+            }
+                Console.WriteLine(strdubl);
+        }
+    }
+}
