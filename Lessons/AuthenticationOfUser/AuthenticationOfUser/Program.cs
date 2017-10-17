1210386f3fd07e4540fade60a6fc0b1fd3856754
LinasBushina <xmysteryshadow@gmail.com>
2017-10-17 21:44:14 +0300

    File changed

--- Lessons/AuthenticationOfUser/AuthenticationOfUser/Program.cs
+++ Lessons/AuthenticationOfUser/AuthenticationOfUser/Program.cs
@@ -10,34 +10,20 @@ namespace AuthenticationOfUser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the login: ");
-            string login = Console.ReadLine();
-            Console.WriteLine("Enter the password: ");
-            string password = Console.ReadLine();
+            string login = "";
+            string password = "";
             string correctlogin = "Fox";
             string correctpassword = "123";
-            while(true)
-            {
-                if(login != correctlogin)
-                 { 
-                    Console.WriteLine("Login is not correct. Try again");
-                 Console.WriteLine("Enter the login: ");
-                  login = Console.ReadLine();
-                 Console.WriteLine("Enter the password: ");
-                 password = Console.ReadLine();
-                 }
-                if (password != correctpassword)
-                { 
-                Console.WriteLine("Password is not correct. Try again");
+            do
+            { 
                 Console.WriteLine("Enter the login: ");
                 login = Console.ReadLine();
                 Console.WriteLine("Enter the password: ");
                 password = Console.ReadLine();
-                }
-                Console.WriteLine("All is okey!");
-                return;
-            }
-
+                if (login != correctlogin || password != correctpassword)
+                { Console.WriteLine("Data is not correct. Try again"); }
+            } while (login != correctlogin || password != correctpassword);
+            Console.WriteLine("All is okey!");
         }
     }
 }
