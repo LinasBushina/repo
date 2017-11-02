41a16f2309c0f0877e9c21914bd9aed291173c84
gologames@bk.ru <1qa2ws3ed>
2017-11-02 21:45:10 +0300

    Maked Huffman algorithm

---
+++ Lessons/Xafmanprogram/Xafmanprogram/HuffmanElement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xafmanprogram
+{
+    class HuffmanElement : IComparable<HuffmanElement>
+    {
+        private bool isBase;
+        private double weight;
+        private HuffmanElement parent1;
+        private HuffmanElement parent2;
+        private string name;
+        private string code;
+
+        private void AddPrefixName(bool isMin)
+        {
+            if (isBase)
+            { code = (isMin ? "0" : "1") + code; }
+            else
+            {
+                parent1.AddPrefixName(isMin);
+                parent2.AddPrefixName(isMin);
+            }
+        }
+
+        public HuffmanElement(string name, double weight)
+        {
+            this.isBase = true;
+            this.name = name;
+            this.weight = weight;
+            this.parent1 = null;
+            this.parent2 = null;
+            this.code = "";
+        }
+        public HuffmanElement(HuffmanElement parent1, HuffmanElement parent2)
+        {
+            this.isBase = false;
+            this.weight = parent1.weight + parent2.weight;
+            HuffmanElement min = parent1.weight < parent2.weight ? parent1 : parent2;
+            HuffmanElement max = parent1.weight >= parent2.weight ? parent1 : parent2;
+            min.AddPrefixName(true);
+            max.AddPrefixName(false);
+            this.parent1 = min;
+            this.parent2 = max;
+        }
+
+        public string GetName()
+        { return name; }
+        public string GetCode()
+        { return code; }
+
+        public int CompareTo(HuffmanElement other)
+        { return other.weight.CompareTo(weight); }
+    }
+}
--- Lessons/Xafmanprogram/Xafmanprogram/Program.cs
+++ Lessons/Xafmanprogram/Xafmanprogram/Program.cs
@@ -8,27 +8,27 @@ namespace Xafmanprogram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void OldSolution()
         {
-            
             Console.WriteLine("Put the quantity of numbers: ");
             int N = Convert.ToInt32(Console.ReadLine());
-            double[] array_x = new double [N];
+            double[] array_x = new double[N];
             Console.WriteLine("Put numbers: ");
-            for(int i = 0; i<N; i++)
+            for (int i = 0; i < N; i++)
             {
                 array_x[i] = Convert.ToDouble(Console.ReadLine());
-                if( array_x[i] >= 1)
+                if (array_x[i] < 0 || array_x[i] >= 1)
                 {
                     Console.WriteLine("Error!");
+                    return;
                 }
             }
             double[] array_x_sort = new double[N];
-            for (int i = 0; i < N; i++ )
+            for (int i = 0; i < N; i++)
             {
                 array_x_sort[i] = array_x[i];
             }
-                Console.WriteLine();
+            Console.WriteLine();
             for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < array_x_sort.Length - 1 - i; j++)
@@ -61,6 +61,34 @@ namespace Xafmanprogram
             for (int i = 0; i < k; i++)
             {
                 Console.Write(array_sum[i] + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<HuffmanElement> lst = new List<HuffmanElement>();
+            lst.Add(new HuffmanElement("A", 0.4));
+            lst.Add(new HuffmanElement("B", 0.2));
+            lst.Add(new HuffmanElement("C", 0.15));
+            lst.Add(new HuffmanElement("D", 0.15));
+            lst.Add(new HuffmanElement("E", 0.1));
+            List<HuffmanElement> cloneLst = new List<HuffmanElement>(lst);
+
+            int count = lst.Count - 1;
+            for (int i = 0; i < count; i++)
+            {
+                lst.Sort();
+                HuffmanElement p1 = lst[lst.Count - 2];
+                HuffmanElement p2 = lst[lst.Count - 1];
+                HuffmanElement el = new HuffmanElement(p1, p2);
+                lst.RemoveAt(lst.Count - 1);
+                lst[lst.Count - 1] = el;
+            }
+
+            for (int i = 0; i < cloneLst.Count; i++)
+            {
+                Console.WriteLine("{0} - {1}",
+                    cloneLst[i].GetName(), cloneLst[i].GetCode());
             }
         }
     }
