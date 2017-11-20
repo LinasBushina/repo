1a720b0e8bb1b39e0f8fec1346de9843e76d954f
LinasBushina <xmysteryshadow@gmail.com>
2017-11-20 20:38:01 +0300

    added new files

--- Lessons/Graphs/Galina_Graph/Galina_Graph/Program.cs
+++ Lessons/Graphs/Galina_Graph/Galina_Graph/Program.cs
@@ -10,11 +10,22 @@ namespace Galina_Graph
     {
         static void Main(string[] args)
         {
-            AdjacencyMatrixGraph g1 = new AdjacencyMatrixGraph(false, 5);
-            Console.WriteLine(Sample_1(g1));
+            //AdjacencyMatrixGraph g1 = new AdjacencyMatrixGraph(false, 5);
+            //Console.WriteLine(Sample_1(g1));
+            //Console.WriteLine();
+            //AdjacencyListGraph g2 = new AdjacencyListGraph(false, 5);
+            //Console.WriteLine(Sample_1(g2));
+            AdjacencyMatrixGraph g3 = new AdjacencyMatrixGraph(false, 6);
+            Console.WriteLine(Sample_2(g3));
             Console.WriteLine();
-            AdjacencyListGraph g2 = new AdjacencyListGraph(false, 5);
-            Console.WriteLine(Sample_1(g2));
+            AdjacencyListGraph g4 = new AdjacencyListGraph(false, 6);
+            Console.WriteLine(Sample_2(g4));
+            Console.WriteLine();
+            AdjacencyMatrixGraph g5 = new AdjacencyMatrixGraph(true, 4);
+            Console.WriteLine(Sample_3(g5));
+            Console.WriteLine();
+            AdjacencyListGraph g6 = new AdjacencyListGraph(true, 4);
+            Console.WriteLine(Sample_3(g6));
         }
 
         static Graph Sample_1(Graph g)
@@ -27,5 +38,27 @@ namespace Galina_Graph
             g.AddEdge(3, 4);
             return g;
         }
+        static Graph Sample_2(Graph g)
+        {
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(0, 5);
+            g.AddEdge(1, 3);
+            g.AddEdge(1, 4);
+            g.AddEdge(2, 4);
+            g.AddEdge(3, 5);
+            g.AddEdge(4, 5);
+            return g;
+        }
+        static Graph Sample_3(Graph g)
+        {
+            g.AddEdge(0, 1);
+            g.AddEdge(1, 3);
+            g.AddEdge(2, 0);
+            g.AddEdge(2, 1);
+            g.AddEdge(3, 2);
+            return g;
+        }
     }
 }
+
---
+++ Lessons/Hamming/Hamming/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamming
+{
+    class Program
+    {
+        static int summ (int a, int b, int c, int f)
+        {
+            if(a + b == 1 || a + b == 0)
+            {
+                a = a + b;
+            }
+            else { a = 0; }
+            if (a + c == 1 || a + c == 0)
+            {
+                a = a + c;
+            }
+            else { a = 0; }
+            if (a + f == 1 || a + f == 0)
+            {
+                a = a + f;
+            }
+            else { a = 0; }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put the quantity of numbers: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[N];
+            Console.WriteLine("Put numbers: ");
+            for (int i = 0; i < N; i++)
+            {
+                if (array[i] == 0 || array[i] == 1)
+                {
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                    return;
+                }
+            }
+            int[] array1 = new int[N];
+            array1[0] = summ (array[0],array[2],array[4],array[6]);
+            array1[1] = summ (array[1],array[2],array[5],array[6]);
+            array1[2] = summ (array[3], array[4], array[5], array[6]);
+            string X = "array[2]+array[1]+array[0]";
+            int numx = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                numx *= 10;
+                numx += X[i] - '0';
+            }
+            int dec = Convert.ToInt32(X, 2);
+            Console.WriteLine(dec);
+        }
+    }
+}
