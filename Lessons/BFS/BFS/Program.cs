845f39e3e26765b39cc576bc46401c483bc628d7
LinasBushina <xmysteryshadow@gmail.com>
2017-11-30 21:47:52 +0300

    added new file

--- Lessons/BFS/BFS/Program.cs
+++ Lessons/BFS/BFS/Program.cs
@@ -10,10 +10,10 @@ namespace BFS
     {
         static void Main(string[] args)
         {
-            Test2();
+            Graph3();
         }
 
-        static void Test1()
+        static void Graph1()
         {
             AdjacencyMatrixGraph g = new AdjacencyMatrixGraph(6);
             g.AddEdge(0, 1); g.AddEdge(0, 2); g.AddEdge(1, 3);
@@ -24,7 +24,7 @@ namespace BFS
             foreach (var node in TraversalGraph.DFS(0, g))
             { Console.WriteLine(node); }
         }
-        static void Test2()
+        static void Graph2()
         {
             AdjacencyMatrixGraph g = new AdjacencyMatrixGraph(6);
             g.AddEdge(0, 1); g.AddEdge(0, 2); g.AddEdge(0, 3);
@@ -32,6 +32,22 @@ namespace BFS
             g.AddEdge(3, 4); g.AddEdge(4, 5);
             foreach (var pair in TraversalGraph.BFSwithTier(0, g))
             { Console.WriteLine("{0} - {1}", pair.Item1, pair.Item2); }
+            Console.WriteLine();
+            foreach (var node in TraversalGraph.DFS(0, g))
+            { Console.WriteLine(node); }
+        }
+        static void Graph3()
+        {
+            AdjacencyMatrixGraph g = new AdjacencyMatrixGraph(6);
+            g.AddEdge(0, 5); g.AddEdge(1, 2); g.AddEdge(1, 4);
+            g.AddEdge(1, 5); g.AddEdge(2, 4); g.AddEdge(2, 5);
+            g.AddEdge(3, 4); g.AddEdge(4, 5);
+            Console.WriteLine("Обход графа в ширину: ");
+            Console.WriteLine();
+            foreach (var pair in TraversalGraph.BFSwithTier(0, g))
+            { Console.WriteLine("{0} - {1}", pair.Item1, pair.Item2); }
+            Console.WriteLine();
+            Console.WriteLine("Обход графа в глубину: ");
             Console.WriteLine();
             foreach (var node in TraversalGraph.DFS(0, g))
             { Console.WriteLine(node); }
