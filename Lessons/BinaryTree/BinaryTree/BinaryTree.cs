54c7be43b9a5379681b074ebf7d16ee32a542fc8
gologames@bk.ru <1qa2ws3ed>
2017-12-09 22:16:17 +0300

    Finished binary tree

--- Lessons/BinaryTree/BinaryTree/BinaryTree.cs
+++ Lessons/BinaryTree/BinaryTree/BinaryTree.cs
@@ -11,7 +11,8 @@ namespace BinaryTree
         private Node root;
         public BinaryTree()
         { this.root = null; }
-
+        
+        //возвращает кортеж из 2 элементов (узел, родитель)
         private Tuple<Node, Node> _Find(int key)
         {
             if (root == null)
@@ -26,14 +27,14 @@ namespace BinaryTree
                     if (current.GetLeft() == null)
                     { return new Tuple<Node, Node>(null, current); }
                     parent = current;
-                    current = current.GetLeft();
+                    current = current.GetLeft(); //спуск
                 }
                 else
                 {
                     if (current.GetRight() == null)
                     { return new Tuple<Node, Node>(null, current); }
                     parent = current;
-                    current = current.GetRight();
+                    current = current.GetRight(); //спуск
                 }
             }
             return new Tuple<Node,Node>(current, parent);
@@ -46,11 +47,122 @@ namespace BinaryTree
 
             if (exNode == null)
             {
-                if (true)
-                {
-                    
-                }
+                if (parent == null) root = node;
+                else if (parent.GetKey() > node.GetKey())
+                { parent.SetLeft(node); }
+                else parent.SetRight(node);
+            }
+            else
+            {
+                node.SetLeft(exNode.GetLeft());
+                node.SetRight(exNode.GetRight());
+                if (parent.GetLeft() == exNode)
+                { parent.SetLeft(node); }
+                else parent.SetRight(node);
             }
         }
+
+        public Node Find(int key)
+        { return _Find(key).Item1; }
+
+        public Node Remove(int key)
+        {
+            Tuple<Node, Node> tup = _Find(key);
+            Node node = tup.Item1, parent = tup.Item2;
+            if (node == null) return null;
+            
+            //leaf case
+            if (node.GetLeft() == null && node.GetRight() == null)
+            {
+                if (parent != null)
+                {
+                    if (parent.GetLeft() == node)
+                    { parent.SetLeft(null); }
+                    else parent.SetRight(null);
+                }
+                else root = null;
+            }
+            //signle child case
+            else if (node.GetLeft() == null || node.GetRight() == null)
+            {
+                bool nodeHasLeft = node.GetLeft() != null;
+                if (parent != null)
+                {
+                    if (parent.GetLeft() == node)
+                    {
+                        if (nodeHasLeft) parent.SetLeft(node.GetLeft());
+                        else parent.SetLeft(node.GetRight());
+                    }
+                    else
+                    {
+                        if (nodeHasLeft) parent.SetRight(node.GetLeft());
+                        else parent.SetRight(node.GetRight());
+                    }
+                }
+                else
+                {
+                    if (nodeHasLeft) root = node.GetLeft();
+                    else root = node.GetRight();
+                }
+            }
+            //two childs case
+            else
+            {
+                Node currParent = node;
+                Node current = node.GetRight();
+                while (current.GetLeft() != null)
+                {
+                    currParent = current; //спуск
+                    current = current.GetLeft();
+                }
+                if (parent != null)
+                {
+                    if (parent.GetLeft() == node)
+                    { parent.SetLeft(current); }
+                    else parent.SetRight(current);
+                }
+                else root = current;
+                current.SetLeft(node.GetLeft());
+                if (currParent != node)
+                {
+                    currParent.SetLeft(current.GetRight());
+                    current.SetRight(node.GetRight());
+                }
+            }
+
+            node.SetLeft(null);
+            node.SetRight(null);
+            return node;
+        }
+
+        private void _PreOrderTraversal(Node node)
+        {
+            if (node == null) return;
+            Console.WriteLine(node);
+            _PreOrderTraversal(node.GetLeft());
+            _PreOrderTraversal(node.GetRight());
+        }
+        public void PreOrderTraversal()
+        { _PreOrderTraversal(root); }
+
+        private void _InOrderTraversal(Node node)
+        {
+            if (node == null) return;
+            _InOrderTraversal(node.GetLeft());
+            Console.WriteLine(node);
+            _InOrderTraversal(node.GetRight());
+        }
+        public void InOrderTraversal()
+        { _InOrderTraversal(root); }
+
+        private void _PostOrderTraversal(Node node)
+        {
+            if (node == null) return;
+            _PostOrderTraversal(node.GetLeft());
+            _PostOrderTraversal(node.GetRight());
+            Console.WriteLine(node);
+        }
+        public void PostOrderTraversal()
+        { _PostOrderTraversal(root); }
     }
 }
--- Lessons/BinaryTree/BinaryTree/Program.cs
+++ Lessons/BinaryTree/BinaryTree/Program.cs
@@ -10,6 +10,24 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
+            BinaryTree tree = new BinaryTree();
+            Node node = new Node(0);
+            tree.Add(node);
+            tree.Add(new Node(3));
+            tree.Add(new Node(2));
+            tree.Remove(0);
+            tree.Add(new Node(7));
+            tree.Add(new Node(9));
+            //Console.WriteLine(tree.Find(0) != null);
+            //Console.WriteLine(tree.Find(2) != null);
+            //Console.WriteLine(tree.Find(7) != null);
+            //Console.WriteLine(tree.Find(3) != null);
+            //Console.WriteLine(tree.Find(8) != null);
+            tree.PreOrderTraversal();
+            Console.WriteLine();
+            tree.InOrderTraversal();
+            Console.WriteLine();
+            tree.PostOrderTraversal();
         }
     }
 }
