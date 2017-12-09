26cd408c9a95c7196cfc5ba977530eea9662b9ef
gologames@bk.ru <1qa2ws3ed>
2017-12-09 21:35:30 +0300

    Added binary tree project

---
+++ Lessons/BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        private Node root;
+        public BinaryTree()
+        { this.root = null; }
+
+        private Tuple<Node, Node> _Find(int key)
+        {
+            if (root == null)
+            { return new Tuple<Node, Node>(null, null); }
+            
+            Node current = root;
+            Node parent = null;
+            while (key != current.GetKey())
+            {
+                if (key < current.GetKey())
+                {
+                    if (current.GetLeft() == null)
+                    { return new Tuple<Node, Node>(null, current); }
+                    parent = current;
+                    current = current.GetLeft();
+                }
+                else
+                {
+                    if (current.GetRight() == null)
+                    { return new Tuple<Node, Node>(null, current); }
+                    parent = current;
+                    current = current.GetRight();
+                }
+            }
+            return new Tuple<Node,Node>(current, parent);
+        }
+
+        public void Add(Node node)
+        {
+            Tuple<Node, Node> tup = _Find(node.GetKey());
+            Node exNode = tup.Item1, parent = tup.Item2;
+
+            if (exNode == null)
+            {
+                if (true)
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Lessons/BinaryTree/BinaryTree/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        private int key;
+        private Node left;
+        private Node right;
+
+        public Node(int key)
+        {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+        }
+
+        public int GetKey()
+        { return key; }
+        public Node GetLeft()
+        { return left; }
+        public Node GetRight()
+        { return right; }
+        public void SetLeft(Node left)
+        { this.left = left; }
+        public void SetRight(Node right)
+        { this.right = right; }
+
+        public override string ToString()
+        { return key.ToString(); }
+    }
+}
