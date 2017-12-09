65c171391f066fbc0b867453dc6f3b029f4873ce
LinasBushina <xmysteryshadow@gmail.com>
2017-12-09 22:37:06 +0300

    modified files

--- Lessons/BinaryTree/BinaryTree/Program.cs
+++ Lessons/BinaryTree/BinaryTree/Program.cs
@@ -8,7 +8,7 @@ namespace BinaryTree
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Test1()
         {
             BinaryTree tree = new BinaryTree();
             Node node = new Node(0);
@@ -28,6 +28,38 @@ namespace BinaryTree
             tree.InOrderTraversal();
             Console.WriteLine();
             tree.PostOrderTraversal();
+        }
+        static void Test2()
+        {
+            BinaryTree tree = new BinaryTree();
+            Node node = new Node(6);
+            tree.Add(node);
+            tree.Add(new Node(2));
+            tree.Add(new Node(8));
+            tree.Add(new Node(1));
+            tree.Add(new Node(4));
+            tree.Add(new Node(7));
+            tree.Add(new Node(9));
+            tree.Add(new Node(3));
+            tree.Add(new Node(5));
+            tree.Remove(2);
+            Console.WriteLine(tree.Find(2) != null);
+            Console.WriteLine(tree.Find(8) != null);
+            Console.WriteLine(tree.Find(1) != null);
+            Console.WriteLine(tree.Find(4) != null);
+            Console.WriteLine(tree.Find(7) != null);
+            Console.WriteLine(tree.Find(9) != null);
+            Console.WriteLine(tree.Find(3) != null);
+            Console.WriteLine(tree.Find(5) != null);
+            tree.PreOrderTraversal();
+            Console.WriteLine();
+            tree.InOrderTraversal();
+            Console.WriteLine();
+            tree.PostOrderTraversal();
+        }
+        static void Main(string[] args)
+        {
+            Test2();
         }
     }
 }
