8a719ece5dc1bbdc2385676d122bce70f6da91ba
LinasBushina <xmysteryshadow@gmail.com>
2018-04-03 22:32:51 +0300

    new comments

--- Lessons/Asm/TranslationNumberSystems/TranslationNumberSystems/Translation.asm
+++ Lessons/Asm/TranslationNumberSystems/TranslationNumberSystems/Translation.asm
@@ -3,27 +3,27 @@ includelib libcmt.lib
 .386
 .model flat, c
 .stack 4096
-printf proto, pString : ptr byte, args : vararg
+printf proto, pString : ptr byte, args : vararg ; обьявляем прототипы функций, возвращаемое зн-е не указ.
 scanf proto, pString : ptr byte, args : ptr byte
-strlen proto, pString : ptr byte
-strcat proto, dest : ptr byte, src : ptr byte
+strlen proto, pString : ptr byte ; вычисляем длину строки т.е. прототип функции
+strcat proto, dest : ptr byte, src : ptr byte ; склеивание строк
 
 .data
-fsEnterNum db "Enter num: ", 0
+fsEnterNum db "Enter num: ", 0 ; для счит и печати
 fsScanfNum db "%s", 0
 fsScanfBase db "%d", 0
 fsEnterFrom db "Enter from number system: ", 0
 fsEnterTo db "Enter to number system: ", 0
 fsPrintNum db "%d", 0dh, 0ah, 0
-numBuf db 20 dup(?)
-from dd ?
-to dd ?
+numBuf db 20 dup(?) ; дубликейт
+from dd ? ; степень из которой переводим в к-е записано число
+to dd ? 
 digit dd ?
 sum dd 0
 pow dd 1
 index dd ?
 len dd ?
-bufflag db 0
+bufflag db 0 
 resbuf_1 db 50 dup(0)
 resbuf_2 db 50 dup(0)
 
@@ -37,13 +37,13 @@ main proc
 	invoke printf, addr fsEnterTo
 	invoke scanf, addr fsScanfBase, addr to
 
-	; len = strlen(numBuf)
+	; len = strlen(numBuf) 
 	invoke strlen, addr numBuf
 	mov [len], eax
 	dec eax
 	mov [index], eax
 
-	radix:
+	radix:                  ; определение символ цифры /символ буквы
 	mov edi, offset numBuf
 	mov ebx, [index]
 	mov eax, [edi + ebx]
@@ -63,43 +63,43 @@ main proc
 	mov eax, [digit]
 	sub eax, 55
 
-	save_digit:
+	save_digit:          ; получаем цифру
 	mov [digit], eax
 
-	; digit * pow^radix
+	; digit * from^radix 
 	mov eax, [digit]
     xor edx, edx
 	mov ebx, [pow]
 	mul ebx			;mul in edx:eax
 
-	; sum += digit * pow^radix
+	; sum += digit * from^radix
 	add eax, [sum]
 	mov [sum], eax
 
-	; pow *= from
+	; pow *= from 
 	mov eax, [pow]
     xor edx, edx
 	mov ebx, [from]
 	mul ebx
 	mov [pow], eax
 
-	mov eax, [index]
+	mov eax, [index] ; движемся к след разряду
 	dec eax
 	mov [index], eax
-	cmp eax, -1
+	cmp eax, -1 ; пока не доконца дошли
 	jne radix
 
-	invoke printf, addr fsPrintNum, sum
+	invoke printf, addr fsPrintNum, sum ; десятичное представление печатаем
 
-	convert_to:
+	convert_to:     ; делим
 	mov eax, [sum]
 	mov ebx, [to]
 	xor edx, edx
 	div ebx									;mod in edx, div in eax
 	
-	mov [sum], eax
+	mov [sum], eax        ; сохраняем 
 	mov [digit], edx
-	cmp edx, 9
+	cmp edx, 9            ; сравниваем
 	jg buf_hex
 
 	buf_dec:
@@ -129,14 +129,14 @@ main proc
 	mov [resbuf_2 + 1], 0
 	invoke strcat, addr resbuf_2, addr resbuf_1
 
-	check_sum:
+	check_sum:  ; инвертируем 
 	mov eax, 1
 	sub al, bufflag
 	mov [bufflag], al
 	
-	mov eax, [sum]
+	mov eax, [sum] ; сравнение 0 -конец
 	cmp eax, 0
-	jne convert_to
+	jne convert_to ; прыгаем обратно
 
 	print_ast_to:
 	cmp bufflag, 0
