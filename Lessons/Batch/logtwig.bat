cca580d290c4231f10b7ccca1312a83cf69d5f83
gologames <gologames@bk.ru>
2018-05-22 23:04:49 +0300

    Some progress batch

--- Lessons/Batch/logtwig.bat
+++ Lessons/Batch/logtwig.bat
@@ -2,14 +2,59 @@
 setlocal enabledelayedexpansion 
 set /a size = 0
 if "%1"=="log" (
-	for /f "tokens=*" %%i in ('set') do (
+	@rem for /f "tokens=*" %%i in ('set') do (
+	for /f "tokens=*" %%i in (testenv.txt) do (
 		for /f "tokens=1,2 delims=^=" %%a in ("%%i") do (
-			set arr[!size!].name = %%a
-			set arr[!size!].value = %%b
+			set arrname[!size!]=%%a
+			set arrvalue[!size!]=%%b
 		)
 		set /a size = !size! + 1
-	  	@rem echo !arr[!size!]!
 	)
-	set arr
+	set /a end_i = !size! - 2
+	for /l %%i in (0, 1, !end_i!) do (
+		set /a end_j = !size! - %%i - 2
+		for /l %%j in (0, 1, !end_j!) do (
+			set /a end_k = %%j + 1
+			for /l %%k in (!end_k!, 1, !end_k!) do (
+				if "%2"=="name" (
+					if "%3"=="aplha" (
+						if !arrname[%%j]! gtr !arrname[%%k]! (
+							set tempname=!arrname[%%j]!
+							set arrname[%%j]=!arrname[%%k]!
+							set arrname[%%k]=!tempname!
+
+							set tempvalue=!arrvalue[%%j]!
+							set arrvalue[%%j]=!arrvalue[%%k]!
+							set arrvalue[%%k]=!tempvalue!
+						)
+					)
+					if "%3"=="length" (
+    					echo !arrname[%%j]!
+						set jstr=!arrname[%%j]!
+						set jlen=0
+						:loop
+    					if defined jstr (set jstr=!jstr:~1!& set /a jlen+=1 & goto:loop)
+    					
+    					@rem echo !arrname[%%j]!=!jlen!
+					)
+				)
+				if "%2"=="value" (
+					if !arrvalue[%%j]! gtr !arrvalue[%%k]! (
+						set tempname=!arrname[%%j]!
+						set arrname[%%j]=!arrname[%%k]!
+						set arrname[%%k]=!tempname!
+
+						set tempvalue=!arrvalue[%%j]!
+						set arrvalue[%%j]=!arrvalue[%%k]!
+						set arrvalue[%%k]=!tempvalue!
+					)
+				)
+			)
+		)
+	)
+	set /a end_i = !size! - 1
+	for /l %%i in (0, 1, !end_i!) do (
+		echo !arrname[%%i]!=!arrvalue[%%i]!
+	)
 )
 endlocal
--- Lessons/Batch/test.bat
+++ Lessons/Batch/test.bat
@@ -1,10 +1,24 @@
 @echo off
 setlocal
+set "myString=abcdef!%%^^()^!"
+call :strlen result myString
+echo %result%
+goto :eof
 
-set "str=string1&with spaces by string2&with spaces.txt"
-
-set "string1=%str: by =" & set "string2=%"
-set "string2=%string2:.txt=%"
-
-echo "%string1%"
-echo "%string2%"
+:strlen <resultVar> <stringVar>
+(   
+    setlocal EnableDelayedExpansion
+    set "s=!%~2!#"
+    set "len=0"
+    for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+        if "!s:~%%P,1!" NEQ "" ( 
+            set /a "len+=%%P"
+            set "s=!s:~%%P!"
+        )
+    )
+)
+( 
+    endlocal
+    set "%~1=%len%"
+    exit /b
+)
