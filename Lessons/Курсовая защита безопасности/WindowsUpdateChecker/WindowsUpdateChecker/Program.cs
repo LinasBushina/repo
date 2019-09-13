1e7ae3b6072d5dbaf66bddfb6948b1285cfa9ee5
gologames <gologames@bk.ru>
2019-09-13 21:49:33 +0300

    Windows Update Checker first steps

---
+++ Lessons/Курсовая защита безопасности/WindowsUpdateChecker/WindowsUpdateChecker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WUApiLib;
+
+namespace WindowsUpdateChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type USType = Type.GetTypeFromProgID("Microsoft.Update.Session");
+            //dynamic updateSession = Activator.CreateInstance(USType);
+            //Type USMType = Type.GetTypeFromProgID("Microsoft.Update.ServiceManager");
+            //dynamic updateServiceManager = Activator.CreateInstance(USMType);
+            //dynamic updateService = updateServiceManager.AddScanPackageService("Offline Sync Service", "c:\\wsusscn2.cab", 1);
+
+            //Console.WriteLine("123");
+
+            UpdateSession uSession = new UpdateSession();
+            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+            uSearcher.Online = false;
+            try
+            {
+                ISearchResult sResult = uSearcher.Search("IsInstalled=1 And IsHidden=0");
+                string text = "Found " + sResult.Updates.Count + " updates" + Environment.NewLine;
+                foreach (IUpdate update in sResult.Updates)
+                {
+                    text += update.Title + Environment.NewLine;
+                }
+                Console.WriteLine(text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.Message);
+            }
+        }
+    }
+}
